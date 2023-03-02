dbf1f6bd5276a9c8931c97d35839ae352c9ed511
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-03-03 03:53:29 +0600

    Modal Added and Put show some dynamic data on modal

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ const loadBlogs = () => {
 };
 
 const displayBLogs = (blogs, showAll) => {
-  console.log(blogs);
   const blogsContainer = document.getElementById("blog-container");
 
   // limited result show in search
@@ -36,7 +35,9 @@ blogsContainer.innerHTML = "";
           <h5>${blog.name}</h5>
           <p class="card-text"><small class="text-muted"><i class="fa-solid fa-calendar-days"></i> ${blog.published_in}</small></p>
           </div>
-          <button type="button" class="btn btn-outline-danger rounded-circle"><i class="fa-solid fa-arrow-right"></i></button>
+          <button  type="button" class="btn btn-outline-danger rounded-circle" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button>
+  
+
       </div>
         </div>
         `;
@@ -53,4 +54,74 @@ blogsContainer.innerHTML = "";
   }
 };
 
+//   load blog details 
+const loadBlogDetails = () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/01`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) =>displayBlogDetails(data.data))
+      } ;
+
+const displayBlogDetails = (blogDetails) => {
+    console.log(blogDetails.input_output_examples[0].input);
+    const modalContainer = document.getElementById('modal-container');
+    const blogDetailsDiv = document.createElement("div");
+    blogDetailsDiv.innerHTML = `
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card h-100">
+        <div class="card-body">
+        <div>
+        <h5>${blogDetails.description}</h5>
+    </div>
+    <div class="d-flex justify-content-between align-items-center">
+        <div class=" p-3">
+            <p>$10/month
+                Basic</p>
+        </div>
+        <div class=" p-3">
+            <p>$10/month
+                Basic</p>
+        </div >
+        <div class="  p-3">
+            <p>$10/month
+                Basic</p>
+        </div>
+    </div>
+    <div class="d-flex justify-content-around align-items-center">
+        <div>
+            <h6>Features</h6>
+            <li>dasd</li>
+            <li>dasd</li>
+            <li>dasd</li>
+        </div>
+        <div>
+            <h6>Integrations</h6>
+            <li>dasd</li>
+            <li>dasd</li>
+            <li>dasd</li>
+        </div>
+    </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="${blogDetails.image_link[1]}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h3 class="text-center">${blogDetails.input_output_examples[0].input}</h3>
+        <p class="text-center">${blogDetails.input_output_examples[0].output}</p>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+    
+    `;
+    modalContainer.appendChild(blogDetailsDiv);
+
+    
+
+};
+loadBlogDetails();
 loadBlogs();
