269753eb64dc5df0a286d1e4633b891f91830b98
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-03-03 01:26:59 +0600

    fetch data,display data,card added

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// fecth api with async and await methods
+const loadBlogs = () => {
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayBLogs(data.data.tools));
+};
+
+const displayBLogs = (blogs) => {
+  console.log(blogs);
+  const blogsContainer = document.getElementById("blog-container");
+
+  // limited result show in search
+  blogs = blogs.slice(0, 6);
+  blogs.forEach((blog) => {
+    const blogDiv = document.createElement("div");
+    blogDiv.classList.add("col");
+    blogDiv.innerHTML = `
+        <div class="card h-100 p-sm-2">
+          <img src="${blog.image}" class="card-img-top p-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Features</h5>
+            <p class="card-text">${blog.features}</p>
+            <ul class="card-text">
+            ${blog.features.map((feature) => `<li>${feature}</li>`).join("")}
+          </ul>
+          </div>
+          <div class="card-footer d-flex justify-content-between align-items-center">
+          <div class="card-text">
+          <h5>${blog.name}</h5>
+          <p class="card-text"><small class="text-muted"><i class="fa-solid fa-calendar-days"></i> ${blog.published_in}</small></p>
+          </div>
+          <button type="button" class="btn btn-outline-danger rounded-circle"><i class="fa-solid fa-arrow-right"></i></button>
+      </div>
+        </div>
+        `;
+    blogsContainer.appendChild(blogDiv);
+  });
+};
+
+loadBlogs();
